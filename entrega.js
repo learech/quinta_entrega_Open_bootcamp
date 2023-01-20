0103ccd654d471bad1ae5ba7bc807c718d7bd211
Lea Rech <learech@hotmail.com>
2023-01-20 18:01:38 -0300

    subiendo

---
+++ entrega.js
@@ -0,0 +1,15 @@
+let alturaCm = 183;
+
+console.log(parseInt (alturaCm));
+
+let alturaMetros = 1.83;
+
+console.log(parseFloat (alturaMetros));
+
+let peso = 83.5
+
+console.log(peso.toFixed(4));
+
+let arriba = (alturaMetros.toFixed(1))
+
+console.log(arriba);
